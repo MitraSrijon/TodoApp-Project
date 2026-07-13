b91925189c830589b3bc29be028e62b78da90cda
apple <mitrasrijon6@gmail.com>
2026-07-13 21:13:33 +0530

    Final commit with the working app

--- src/main/java/app/example/todoApp/Controller/TaskController.java
+++ src/main/java/app/example/todoApp/Controller/TaskController.java
@@ -1,7 +1,17 @@
 package app.example.todoApp.Controller;
 
+import app.example.todoApp.Modal.task;
 import app.example.todoApp.Service.TaskService;
+import org.apache.catalina.LifecycleState;
+import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class TaskController {
@@ -10,5 +20,34 @@ public class TaskController {
 
     public TaskController(TaskService service) {
         this.service = service;
+    }
+
+    //Gives the list of all the tasks that are present in our database...
+    @GetMapping
+    public String getTasks(Model model){
+        List<task> tasks = service.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+
+    //Adding tasks in the database...
+    @PostMapping
+    public String createTask(@RequestParam String title){
+        service.creatATask(title);
+        return "redirect:/"; //It will basically refresh the page once task is added
+    }
+
+    //Logic to delete a task
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable long id){
+        service.deleteTask(id);
+        return "redirect:/";
+    }
+
+    //Logic to change the completion status of the task
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable long id){
+        service.toggleTask(id);
+        return "redirect:/";
     }
 }
--- src/main/java/app/example/todoApp/Service/TaskService.java
+++ src/main/java/app/example/todoApp/Service/TaskService.java
@@ -1,10 +1,44 @@
 package app.example.todoApp.Service;
 
+import app.example.todoApp.Modal.task;
+import app.example.todoApp.Repository.taskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
 
+    private final taskRepository repository;
 
+    public TaskService(taskRepository repository) {
+        this.repository = repository;
+    }
 
+    //Getting all the tasks
+    public List<task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    //Creating a task and saving it inside database
+    public void creatATask(String title) {
+        task task = new task();
+        task.setTask(title);
+        task.setCompleted(false);
+        repository.save(task);
+    }
+
+    //Logic to delete a task from the database...
+    public void deleteTask(long id) {
+        repository.deleteById(id);
+    }
+
+    //Logic to update the ttask completion status
+    public void toggleTask(long id) {
+        task task = repository.findById(id)
+                .orElseThrow( () -> new IllegalArgumentException("Invalid Task ID"));
+        task.setCompleted(!task.isCompleted());
+        repository.save(task);
+    }
 }
+
